15e8af302e88984649645f4b165195a29373d6e3
Adam  Shappy <ajs11300@gmail.com>
2019-12-25 23:26:26 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -1,6 +1,6 @@
 def roll_call_dwarves(array)
   # Your code here
-  array.each_with_index {|index, item| 
+  array.each_with_index {|index, item| p 
 end
 
 def summon_captain_planet# code an argument here
