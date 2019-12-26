a5c74d43f6cda5152e7990b307ae95ac0ab45ae7
Adam  Shappy <ajs11300@gmail.com>
2019-12-26 00:11:34 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -11,9 +11,8 @@ end
 def long_planeteer_calls(array)
   # Your code here
   b = array.find {|n| n.size >= 4}
-  # if b != nil 
-  #   return true
-  b 
+   if b != nil 
+     p "bob"
 end
 
 def find_the_cheese# code an argument here
